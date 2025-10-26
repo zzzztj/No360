56032dabea792d159d82f3e2fda5a3ffb21a7666
zzzztj <zzzztj@hotmail.com>
2025-10-26 18:25:35 +0800

    Refactor service structure

---
+++ Configuration/RefereeSettings.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace No360.Configuration;
+
+public sealed class RefereeSettings
+{
+    public RefereeSettings(IConfiguration configuration)
+    {
+        QuarantineDirectory = Expand(configuration["QuarantineDir"] ??
+                                     @"C:\\ProgramData\\InstallReferee\\Quarantine");
+        Directory.CreateDirectory(QuarantineDirectory);
+
+        WatchFolders = (configuration.GetSection("WatchFolders").Get<string[]>() ?? Array.Empty<string>())
+            .Select(Expand)
+            .Distinct(StringComparer.OrdinalIgnoreCase)
+            .ToArray();
+
+        foreach (var folder in WatchFolders)
+        {
+            Directory.CreateDirectory(folder);
+        }
+    }
+
+    public string QuarantineDirectory { get; }
+
+    public string[] WatchFolders { get; }
+
+    private static string Expand(string value)
+    {
+        return Environment.ExpandEnvironmentVariables(value);
+    }
+}
